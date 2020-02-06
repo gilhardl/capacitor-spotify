c5e6b39517e286e9117664e244a8775a6bfe9667
Lucas GILHARD <l.gilhard@cyberg.fr>
2020-02-06 16:12:15 +0100

    add fake web implementation

--- src/web.ts
+++ src/web.ts
@@ -1,12 +1,128 @@
-import { WebPlugin } from '@capacitor/core';
-import { SpotifySDKPlugin } from './definitions';
+import { WebPlugin } from "@capacitor/core";
+import { SpotifySDKPlugin, CrossfadeState, PlayerState } from "./definitions";
+
+const errorMessage = "Spotify SDK isn't supported on the web";
 
 export class SpotifySDKWeb extends WebPlugin implements SpotifySDKPlugin {
   constructor() {
     super({
-      name: 'SpotifySDK',
-      platforms: ['web']
+      name: "SpotifySDK",
+      platforms: ["web"]
     });
+  }
+
+  async initializeAppRemote(options: {
+    clientId: string;
+    redirectUri: string;
+  }): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async connectToAppRemote(): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async disconnectFromAppRemote(): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async getCrossfadeState(): Promise<{ crossfadeState: CrossfadeState }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async getPlayerState(): Promise<{ playerState: PlayerState }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  subscribeToPlayerState(): void {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  subscribeToPlayerContext(): void {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async play(options: { uri: string }): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async pause(): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async resume(): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async queue(options: { uri: string }): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async skipPrevious(): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async skipNext(): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async skipToIndex(options: {
+    uri: string;
+    index: number;
+  }): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async seekTo(options: {
+    milliseconds: number;
+  }): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async seekToRelativePosition(options: {
+    milliseconds: number;
+  }): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async setRepeat(options: {
+    repeatMode: number;
+  }): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async setShuffle(options: {
+    enabled: boolean;
+  }): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async toggleRepeat(): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+
+  async toggleShuffle(): Promise<{ result: boolean }> {
+    console.error(errorMessage);
+    throw errorMessage;
   }
 }
 
@@ -14,5 +130,5 @@ const SpotifySDK = new SpotifySDKWeb();
 
 export { SpotifySDK };
 
-import { registerWebPlugin } from '@capacitor/core';
+import { registerWebPlugin } from "@capacitor/core";
 registerWebPlugin(SpotifySDK);
