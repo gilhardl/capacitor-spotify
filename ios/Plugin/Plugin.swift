4d752c37ae43f71827ae126c0c46aea9d15f26d4
CYBERG <cyberg@Mac-mini-de-CYBERG.local>
2020-02-11 14:56:47 +0100

    implements initialize method

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -6,4 +6,22 @@ import Capacitor
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
  */
 @objc(SpotifySDK)
-public class SpotifySDK: CAPPlugin {}
+public class SpotifySDK: CAPPlugin {
+    internal static var CLIENT_ID: String = ""
+    internal static var REDIRECT_URI: String = ""
+    
+    @objc func initialize(_ call: CAPPluginCall) {
+        let clientId = call.getString("clientId") ?? ""
+        let redirectUri = call.getString("redirectUri") ?? ""
+
+        if (clientId == "" || redirectUri == "") {
+            call.reject("Client ID or redirect URI missing");
+            return;
+        }
+
+        SpotifySDK.CLIENT_ID = clientId
+        SpotifySDK.REDIRECT_URI = redirectUri
+
+        call.resolve(["result": true]);
+    }
+}
